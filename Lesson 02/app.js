e4e91f20f38c23263b0aeb336c1cbd4bb268f899
Dmitry <dmitry.wide.91@gmail.com>
2024-12-25 14:27:58 +0300

    Создание папки lesson 02

---
+++ Lesson 02/app.js
@@ -0,0 +1,6 @@
+const firstName = 'Dmitry'
+const sureName = 'Wide'
+const age = '33'
+
+
+console.log('Меня зовут', firstName, sureName,'. Мне', age, 'года.')
--- app.js
+++
@@ -1,6 +0,0 @@
-const firstName = 'Dmitry'
-const sureName = 'Wide'
-const age = '33'
-
-
-console.log('Меня зовут', firstName, sureName,'. Мне', age, 'года.')
