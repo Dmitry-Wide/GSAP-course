d542b388380ef2d4a2eaff6aba345fdb504824a9
Dmitry <dmitry.wide.91@gmail.com>
2024-12-25 14:41:27 +0300

    Добавление урока 03

---
+++ Lesson 03/app.js
@@ -0,0 +1,8 @@
+let titleProject = 'Оценка стоимости';
+let screensValue = '"шаблонные, с уникальным дизайном, с анимациями"';
+let screenPrice = 1234;
+let percentage = 56;
+let fullPrice = 9876;
+let responsive = true;
+
+console.log(typeof responsive)
